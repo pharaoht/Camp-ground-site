341805576ebf5a867e4299f4ce676d16a22b99c9
Pharaoh <pharaohmanson@gmail.com>
2020-04-23 19:21:33 -0400

    Added login page

--- app.js
+++ app.js
@@ -150,7 +150,25 @@ app.get("/register", function(req,res){
 //handle sign up logic
 app.post("/register", function(req,res){
   var newUser = new User({username: req.body.username});
-  User.register()
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err)
+      return res.render("register");
+    }
+    passport.authenticate("local")(req,res, function(){
+      res.redirect("/campgrounds");
+    })
+  })
+})
+
+//show Login
+app.get("/login", function(req, res){
+  res.render("login");
+})
+
+//handle login logic
+app.post("/login", function (req , res){
+  res.send("We are logging you in.. ")
 })
 
 
